fb0d625826d6011a008f86f779e5b3b567c00da3
Ian Maher <ianpmaher@gmail.com>
2024-09-04 10:13:16 -0400

    styling background pattern

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,16 +4,18 @@ import GPACalculator from "../components/GPACalculator";
 
 const HomePage = () => {
   return (
-    <div className="dark:bg-dracula-bg bg-paper-bg bg-opacity-90 rounded-2xl text-paper-text dark:text-dracula-text font-karla text-center min-h-screen flex flex-col w-screen my-0 mx-auto">
-      {/* <div className="flex flex-col justify-center ">
+    <div className="w-full h-screen waves-maroon">
+      <div className="dark:bg-dracula-bg bg-paper-bg bg-opacity-90 rounded-2xl text-paper-text dark:text-dracula-text font-karla text-center h-[80vh] flex flex-col w-[90vw] my-0 mx-auto">
+        {/* <div className="flex flex-col justify-center ">
         <Header className="flex flex-col items-center justify-center mx-auto my-2 gap-4" />
-      </div> */}
-      <div className="mx-auto my-0 center-center text-center">
-        <div className="bg-paper-bg text-paper-text h-full md:w-full mx-auto my-8 self-center flex justify-center rounded-md">
-          <GPACalculator />
+        </div> */}
+        <div className="mx-auto my-0 center-center text-center">
+          <div className="bg-paper-bg text-paper-text h-full md:w-full mx-auto my-8 self-center flex justify-center rounded-md">
+            <GPACalculator />
+          </div>
         </div>
+        <Footer className="fixed bottom-0 right-0 bg-dracula-blue text-black p-1 rounded-lg" />
       </div>
-      <Footer className="fixed bottom-0 right-0 bg-dracula-blue text-black p-1 rounded-lg" />
     </div>
   );
 };
